8c158fe84ff6343cc14b14fa8f6ae571fe9d5e34
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-02-25 00:28:06 +0530

    added whereClause :white_check_mark:

--- SqlQueryGenerator.py
+++ SqlQueryGenerator.py
@@ -9,24 +9,36 @@ print('*********************************************')
 time.sleep(2)
 print('\n')
 print('Select a Option')
-Selectquery = int(input('\n 1) SELECT \n 2) INSERT IN \n 3) DELETE \n 4) UPDATE \n 5) ORDER BY \n\n SelectedQuery = '))
+Selectquery = int(input(
+    '\n 1) SELECT \n 2) INSERT IN \n 3) DELETE \n 4) UPDATE \n 5) ORDER BY \n\n SelectedQuery = '))
 
 
 def GlobalTableName():
     global tableName
     tableName = str(input('\nEnter Table Name : '))
-    
-            
+
+
+def addWhereClause():
+    whereClause = "WHERE"
+
+
+def SelectQueryFunct():
+    print(' \nSelect Option \n')
+    option = int(
+        input('1) Specify Column \n2) From All\n\n SelectedOption = '))
+    if (option == 1):
+        ColumnName = str(input('\nEnter the Column Name : '))
+
+        print('\nYour Final Query is \n')
+        print(f'SELECT {ColumnName} FROM {tableName}; \n')
+    else:
+        print(f'SELECT * FROM {tableName}; \n')
+
+
 def proceedQuery(Selectquery):
     if (Selectquery == 1):
         GlobalTableName()
-        print(' \nSelect Option \n')
-        option = int(input('1) Specify Column \n2) From All\n\n SelectedOption = '))
-        if(option == 1):
-            ColumnName = str(input('\nEnter the Column Name : '))
-            print('\nYour Final Query is \n')
-            print(f'SELECT {ColumnName} FROM {tableName}; \n')
-                
-            
-            
+        SelectQueryFunct()
+
+
 proceedQuery(Selectquery)
