ed16dfb0b8dfeb35d2dccc04029e822ab90b7ecf
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-02-24 19:31:26 +0530

    Select query Done :white_check_mark:

--- SqlQueryGenerator.py
+++ SqlQueryGenerator.py
@@ -1,7 +1,6 @@
 import time
 
 
-
 print('*********************************************')
 print('\n')
 print('\tWelcome To SQL Query Generator')
@@ -9,16 +8,25 @@ print('\n')
 print('*********************************************')
 time.sleep(2)
 print('\n')
-
-
 print('Select a Option')
 Selectquery = int(input('\n 1) SELECT \n 2) INSERT IN \n 3) DELETE \n 4) UPDATE \n 5) ORDER BY \n'))
+print('\n')
 
-
+def GlobalTableName():
+    global tableName
+    tableName = str(input('Enter Table Name :'))
+    
+            
 def proceedQuery(Selectquery):
     if (Selectquery == 1):
-        option = int(input('Select an Option : \n 1) Specify a Table Name \n 2) Select from all Tables'))
-        tableName = str(input('Enter Table Name :'))
-
-
+        GlobalTableName()
+        print(' \nSelect Option')
+        print('1) Specify Column \n2) From All ')
+        option = int(input())
+        if(option == 1):
+            ColumnName = str(input('Enter the Column Name :'))
+            print(f'SELECT {ColumnName} FROM {tableName};')
+                
+            
+            
 proceedQuery(Selectquery)
