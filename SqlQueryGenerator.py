3af8e7535617b0a1a352e72fb4c363155db9f805
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-02-26 20:52:52 +0530

    Added AND/OR (Not completed)

--- SqlQueryGenerator.py
+++ SqlQueryGenerator.py
@@ -14,6 +14,13 @@ Selectquery = int(input('\n 1) SELECT \n 2) INSERT IN \n 3) DELETE \n 4) UPDATE 
 
 global SelectWhereClause
 
+
+def CoditionalStateMents():
+    # print('Do You Want to Use COnditional Statements? \n 1) Yes \n2) No \n')
+    global SelectconditionalStatement
+    SelectconditionalStatement = int(input("1) AND \n2) OR \n Select a Condition : "))
+        
+    
 def ThankYouMsg():
     print('*********************************************')
     print('\n')
@@ -30,17 +37,30 @@ def addWhereClause():
     global whereClause
     print("\nEnter the Condition\nExample: name = 'John' \n")
     whereClause = str(input("Condition = "))
-
+def WhereClauseAND():
+    global AND
+    print("Enter The Conditions For AND Conditional Statement giving a space between If Multiple \n")
+    AND = input()
 def SelectQueryFunct():
     print(' \nSelect Option \n')
     option = int(input('1) Specify Column \n2) From All\n\n SelectedOption = '))
     if (option == 1):
         ColumnName = str(input('\nEnter the Column Name : '))
+        
         SelectWhereClause = int(input("\nDo You Want To Include Where Clause ? \n 1) YES 2) NO \n Selected Option : "))
         if(SelectWhereClause == 1):
-            addWhereClause()
-            print('\nYour Final Query is \n')
-            print(f'SELECT {ColumnName} FROM {tableName} WHERE {whereClause}; \n')
+            print('Do You Want to Use Conditional Statements? \n 1) Yes \n2) No \n')
+            ConditionOption = int(input())
+            if ConditionOption == 1:
+                CoditionalStateMents()
+                if SelectconditionalStatement == 1:
+                    addWhereClause()
+                    print('\nYour Final Query is \n')
+                    print(f'SELECT {ColumnName} FROM {tableName} WHERE {whereClause} AND ; \n')
+            else:    
+                addWhereClause()
+                print('\nYour Final Query is \n')
+                print(f'SELECT {ColumnName} FROM {tableName} WHERE {whereClause}; \n')
             ThankYouMsg()
         
         
@@ -63,5 +83,6 @@ def proceedQuery(Selectquery):
     elif (Selectquery == 6):
         print("\n")
         ThankYouMsg()
+
 
 proceedQuery(Selectquery)
